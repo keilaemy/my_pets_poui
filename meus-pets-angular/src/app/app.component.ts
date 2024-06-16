0a81b94604e626b5186fd2b8f4d53f3dbdde2a64
keilaemy <keilaemy@gmail.com>
2024-06-16 17:00:51 -0300

    Add alterações, inicio angular

---
+++ meus-pets-angular/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import {
+  PoMenuItem,
+  PoMenuModule,
+  PoPageModule,
+  PoToolbarModule,
+  PoButtonModule,
+} from '@po-ui/ng-components';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    PoToolbarModule,
+    PoMenuModule,
+    PoButtonModule,
+    PoPageModule,
+    HttpClientModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) },
+  ];
+
+  private onClick() {
+    alert('Clicked in menu item');
+  }
+}
